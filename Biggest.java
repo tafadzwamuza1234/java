580c06ecb52e93178ef97b5084b86eb5b30d9bd6
tafadzwamuza1234 <tafadzwamuza41@gmail.com>
2025-12-19 11:14:28 +0530

    Add files via upload

---
+++ Biggest.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class Biggest
+{
+    public static void main(String args[])
+    {
+        int a=10,b=23,c=4;
+        if(a>b && a>c)
+        
+            System.out.println("a is the biggest");
+        
+        else if(b>c)
+        
+            System.out.println("b is the biggest");
+        
+        else
+        
+            System.out.println("c ic the biggest");
+        
+    }
+}
