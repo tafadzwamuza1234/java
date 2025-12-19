2f1e1d0c1f9a80fc7981dd937b2f577f8d433004
tafadzwamuza1234 <tafadzwamuza41@gmail.com>
2025-12-19 11:52:20 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Simple
+{
+    public static void main(String args[])
+    {
+        int p=10000,r=20,t=2;
+        float si=(p*r*t)/100;
+
+        System.out.println("principal is"+ p);
+        System.out.println("the rate is "+ r);
+        System.out.println("the si is " + si );
+    }
+}
