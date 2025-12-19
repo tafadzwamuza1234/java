53b51f830ee948611aefcee6078d445ea287d164
tafadzwamuza1234 <tafadzwamuza41@gmail.com>
2025-12-19 10:57:44 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class Arithmetic
+{
+    public static void main(String args[])
+    {
+        int a=20,b=10;
+        int sum=a+b;
+        int diff=a-b;
+        int div=a/b;
+        int mult=a*b;
+        
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(div);
+        System.out.println(mult);
+    }
+}
